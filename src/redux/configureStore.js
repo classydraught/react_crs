ceac7cffb63c0a335084f71468fbe63ec3fe9b15
draught <draught@draughts-MacBook-Pro.local>
2020-03-27 20:37:53 +0530

    combineReducers part-1

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,17 @@
-import { createStore } from 'redux';
-import { Reducer, intialstate } from './reducer';
+import { createStore, combineReducers } from 'redux';
+import { Dishes } from './dishes';
+import { Comments } from './comments';
+import { Leaders } from './leaders';
+import { Promotions } from './promotions';
 
 export const ConfigureStore = () => {
-    const store = createStore(
-        Reducer, intialstate
-    );
-    return store;
-}
+  const store = createStore(
+    combineReducers({
+      dishes: Dishes,
+      comments: Comments,
+      promotions: Promotions,
+      leaders: Leaders,
+    })
+  );
+  return store;
+};
